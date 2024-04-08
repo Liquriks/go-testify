cb4efcb62e4c16dbe83deb26f05c6ad68e5a9254
Кирилл <kirill.sergd@yandex.ru>
2024-04-09 01:56:18 +0300

    second iteration

--- precode_test.go
+++ precode_test.go
@@ -8,9 +8,10 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func getCountAndCity(req *http.Request) *httptest.ResponseRecorder {
+func getCafes(req *http.Request) *httptest.ResponseRecorder {
 	responseRecorder := httptest.NewRecorder()
 	handler := http.HandlerFunc(mainHandle)
 	handler.ServeHTTP(responseRecorder, req)
@@ -19,26 +20,25 @@ func getCountAndCity(req *http.Request) *httptest.ResponseRecorder {
 
 func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
 	totalCount := 4
-	count := 5
-	city := "moscow"
-	responseRecorder := getCountAndCity(httptest.NewRequest(http.MethodGet, fmt.Sprintf("/cafe?count=%d&city=%s", count, city), nil))
+	responseRecorder := getCafes(httptest.NewRequest(http.MethodGet, fmt.Sprintf("/cafe?count=5&city=moscow"), nil))
 
-	assert.Equal(t, []byte(strings.Join(cafeList["moscow"][:totalCount], ",")), responseRecorder.Body.Bytes())
+	assert.Equal(t, (strings.Join(cafeList["moscow"][:totalCount], ",")), responseRecorder.Body.Bytes())
+	body := responseRecorder.Body.String()
+	assert.Equal(t, "wrong city value", body)
 
 }
 
 func TestMainHandlerWhenOk(t *testing.T) {
-	count := 2
-	city := "moscow"
-	responseRecorder := getCountAndCity(httptest.NewRequest(http.MethodGet, fmt.Sprintf("/cafe?count=%d&city=%s", count, city), nil))
+
+	responseRecorder := getCafes(httptest.NewRequest(http.MethodGet, fmt.Sprintf("/cafe?count=2&city=moscow"), nil))
+	require.NotEmpty(t, responseRecorder.Code)
 	assert.Equal(t, http.StatusOK, responseRecorder.Code)
-	assert.Equal(t, []byte(strings.Join(cafeList["moscow"][:count], ",")), responseRecorder.Body.Bytes())
+	assert.Equal(t, (strings.Join(cafeList["moscow"][:2], ",")), responseRecorder.Body.Bytes())
 }
 
 func TestWhenWrongCity(t *testing.T) {
-	count := 2
-	city := "london"
-	responseRecorder := getCountAndCity(httptest.NewRequest(http.MethodGet, fmt.Sprintf("/cafe?count=%d&city=%s", count, city), nil))
+
+	responseRecorder := getCafes(httptest.NewRequest(http.MethodGet, fmt.Sprintf("/cafe?count=2&city=london"), nil))
 	assert.Equal(t, responseRecorder.Code, http.StatusBadRequest)
-	assert.Equal(t, []byte("wrong city value"), responseRecorder.Body.Bytes())
+	assert.Equal(t, ("wrong city value"), responseRecorder.Body.String())
 }
